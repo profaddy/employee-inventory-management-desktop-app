dd252e420b0a4bb8206104f0da4dbe42409a2d5e
profaddy <adnansaify11@gmail.com>
2020-10-08 08:44:57 +0530

    upated at 8th oct

--- server/node-service-install.js
+++ server/node-service-install.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
  
 // Create a new service object
 var svc = new Service({
-  name:'inventory-node-server',
+  name:'inventory-node-server-1',
   description: 'This is inventory management node server',
   script: '.\\server.js',
   nodeOptions: [
--- server/node-service-uninstall.js
+++ server/node-service-uninstall.js
@@ -2,7 +2,7 @@ var Service = require('node-windows').Service;
  
 // Create a new service object
 var svc = new Service({
-  name:'inventory-node-server',
+  name:'inventory-node-server-1',
   script: ".\\server.js"
 });
  
